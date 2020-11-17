b04f7939a540e556c3433b7df51e96709b2c5eef
Xander Neuwirth <neuwirtha@ad.msoe.edu>
2020-11-16 21:43:03 -0600

    cleanup and prepare to shape data

--- main.py
+++ main.py
@@ -4,21 +4,24 @@ from spectral import *
 from PIL import Image
 import time
 
-classes = ["epithelium", "stroma", "necrosis", "blood", "lymphocytes"]
-
 start = time.time()
-def current(): return round(time.time() - start, 2)
+def log(t): print(f"[{round(time.time() - start, 2)}] {t}.")
 
-print(f"{current()} Loaded imports.")
 
+log("Finished imports.")
+
+# Data loading
+classes = ["epithelium", "stroma", "necrosis", "blood", "lymphocytes"]
 masks = {}
 for tissue in classes:
     for set in ["train", "test"]:
         masks[f"{tissue}_{set}"] = np.asarray(Image.open(f"/data/berisha_lab/neuwirth/annotations-1-masks/ov-63-hd-16ca-{tissue}-{set}.bmp"))
-        print(f"{current()} Loaded {tissue}_{set}.")
-
-print(f"{current()} Finished loading masks.")
+        log(f"Loaded {tissue}_{set}.")
+log("Finished loading masks.")
 
 OV63 = '/data/berisha_lab/ftir/with-paraffin/ov-63/hd/16ca/ov-63-hd-16ca.hdr'
 img = open_image(OV63)
-print(f"{current()} Loaded imaging file.")
+log("Finished loading data.")
+
+print(masks["epithelium_train"].shape)
+print(img.shape)
