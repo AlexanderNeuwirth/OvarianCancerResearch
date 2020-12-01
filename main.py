5effc8586c60a9842ac6838c1efee6ef674dbadd
Xander Neuwirth <neuwirtha@ad.msoe.edu>
2020-11-30 23:37:56 -0600

    label autojob

--- main.py
+++ main.py
@@ -15,6 +15,7 @@ masks = {}
 for tissue in classes:
     for set in ["train", "test"]:
         masks[f"{tissue}_{set}"] = np.asarray(Image.open(f"/data/berisha_lab/neuwirth/annotations-1-masks/{tissue}_{set}.png"))
+        masks[f"{tissue}_{set}"][masks[f"{tissue}_{set}"] > 0] = 1
         print(f"{current()} Loaded {tissue}_{set}.")
 
 print(f"{current()} Finished loading masks.")
@@ -22,5 +23,12 @@ print(f"{current()} Finished loading masks.")
 OV63 = '/data/berisha_lab/ftir/with-paraffin/ov-63/hd/16ca/ov-63-hd-16ca.hdr'
 img = open_image(OV63)
 print(f"{current()} Loaded imaging file.")
-print(img.shape)
-print(masks["epithelium_train"].shape)
+y = np.zeros_like(masks["epithelium_train"])
+i = 1
+
+for i in len(masks):
+    mask = masks[i]
+    y[mask == 1] = i
+    i += 1
+
+print(np.count(y))
